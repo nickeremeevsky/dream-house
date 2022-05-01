26751e975c4c215b322a21af39f4bade5756d09d
Nick Eremeevsky <nick.eremeevsky@yandex.ru>
2022-05-01 03:28:17 +0300

    create header and home section, add styles and img

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,11 @@
 import 'scrollreveal';
 import 'boxicons';
+
+// change background header
+function scrollHeader() {
+  const header = document.getElementById('header');
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
