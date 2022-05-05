2b5b2a06f988ca5e815f89ebf384142d36900ce4
Nick Eremeevsky <nick.eremeevsky@yandex.ru>
2022-05-05 14:02:37 +0300

    add accordion func

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,63 @@
-import 'scrollreveal';
-import 'boxicons';
+import Swiper from 'swiper/bundle';
 
 // change background header
 function scrollHeader() {
   const header = document.getElementById('header');
   // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 50) header.classList.add('scroll-header');
-  else header.classList.remove('scroll-header');
+  if (this.scrollY >= 50) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
 }
 window.addEventListener('scroll', scrollHeader);
+
+// slider
+let swiperPopular = new Swiper('.popular__container', {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+let swiperPopularCard = new Swiper('.imageSwiper', {
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+// value accordion
+const accordionItems = document.querySelectorAll('.value__accordion-item');
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.value__accordion-header');
+
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open');
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.value__accordion-content');
+
+  if (item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style');
+    item.classList.remove('accordion-open');
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    item.classList.add('accordion-open');
+  }
+};
